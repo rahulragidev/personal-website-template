39e392b608f363890e6366e62a15326e24b4ed3e
rahulragidev <iamrahulragi@gmail.com>
2026-01-23 23:06:52 +0530

    Add Photos component to display a gallery of images, update Page component to include site name and Photos section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import { ComponentExample } from "@/components/component-example";
 import { Container } from "@/components/container";
+import { Photos } from "@/components/photos";
+import { siteConfig } from "@/lib/site-config";
 
 export default function Page() {
 	return (
-		<Container>
-			<ComponentExample />
-		</Container>
+		<>
+			<Container>
+				<h1>{siteConfig.name}</h1>
+			</Container>
+			<Photos />
+			<Container>
+				<ComponentExample />
+			</Container>
+		</>
 	);
 }
---
+++ components/photos.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import image1 from "@/public/image1.jpg";
+import image2 from "@/public/image2.jpg";
+import image3 from "@/public/image3.jpg";
+import image4 from "@/public/image4.jpg";
+import image5 from "@/public/image5.jpg";
+
+const images = [image1, image2, image3, image4, image5] as const;
+
+const rotations = [
+    "rotate-2",
+    "-rotate-2",
+    "rotate-2",
+    "rotate-2",
+    "-rotate-2",
+] as const;
+
+export function Photos() {
+    return (
+        <div className="mt-16 sm:mt-20">
+            <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
+                {images.map((image, imageIndex) => (
+                    <div
+                        key={image.src}
+                        className={cn(
+                            "relative w-44 flex-none overflow-hidden rounded-xl bg-card sm:w-72 sm:rounded-2xl",
+                            rotations[imageIndex % rotations.length],
+                        )}
+                    >
+                        <div className="aspect-9/10">
+                            <Image
+                                src={image}
+                                alt={`Photo ${imageIndex + 1}`}
+                                sizes="(min-width: 640px) 18rem, 11rem"
+                                className="absolute inset-0 h-full w-full object-cover"
+                            />
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
